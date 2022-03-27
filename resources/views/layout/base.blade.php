ba850e58980898b91688ed7c1cae94e933e659a7
UNIWARE <hunter_tigre1998@hotmail.com>
2022-03-27 17:17:22 -0400

    "secure_asset configuration"

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <title>@yield('title', config('app.name'))</title>
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/custom.css') }}">
     @yield('styles')
 </head>
 
